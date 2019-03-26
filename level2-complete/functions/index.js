80c80529929404c83ab0ec712149bfc6fe6b4f7d
cristiespi3 <48964847+cristiespi3@users.noreply.github.com>
2019-03-26 15:53:44 -0400

    Update index.js

--- level2-complete/functions/index.js
+++ level2-complete/functions/index.js
@@ -15,11 +15,12 @@
 
 // Import the Dialogflow module and response creation dependencies
 // from the Actions on Google client library.
+
 const {
   dialogflow,
-  BasicCard,
   Permission,
   Suggestions,
+  BasicCard,
 } = require('actions-on-google');
 
 // Import the firebase-functions package for deployment.
@@ -86,6 +87,7 @@ app.intent('actions_intent_PERMISSION', (conv, params, permissionGranted) => {
 
 // Handle the Dialogflow intent named 'favorite color'.
 // The intent collects a parameter named 'color'.
+
 app.intent('favorite color', (conv, {color}) => {
   const luckyNumber = color.length;
   const audioSound = 'https://actions.google.com/sounds/v1/cartoon/clang_and_wobble.ogg';
@@ -103,7 +105,6 @@ app.intent('favorite color', (conv, {color}) => {
     conv.ask(new Suggestions('Yes', 'No'));
   }
 });
-
 // Handle the Dialogflow intent named 'favorite fake color'.
 // The intent collects a parameter named 'fakeColor'.
 app.intent('favorite fake color', (conv, {fakeColor}) => {
@@ -113,3 +114,40 @@ app.intent('favorite fake color', (conv, {fakeColor}) => {
 
 // Set the DialogflowApp object to handle the HTTPS POST request.
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
+// Define a mapping of fake color strings to basic card objects.
+const colorMap = {
+  'indigo taco': {
+    title: 'Indigo Taco',
+    text: 'Indigo Taco is a subtle bluish tone.',
+    image: {
+      url: 'https://storage.googleapis.com/material-design/publish/material_v_12/assets/0BxFyKV4eeNjDN1JRbF9ZMHZsa1k/style-color-uiapplication-palette1.png',
+      accessibilityText: 'Indigo Taco Color',
+    },
+    display: 'WHITE',
+  },
+  'pink unicorn': {
+    title: 'Pink Unicorn',
+    text: 'Pink Unicorn is an imaginative reddish hue.',
+    image: {
+      url: 'https://storage.googleapis.com/material-design/publish/material_v_12/assets/0BxFyKV4eeNjDbFVfTXpoaEE5Vzg/style-color-uiapplication-palette2.png',
+      accessibilityText: 'Pink Unicorn Color',
+    },
+    display: 'WHITE',
+  },
+  'blue grey coffee': {
+    title: 'Blue Grey Coffee',
+    text: 'Calling out to rainy days, Blue Grey Coffee brings to mind your favorite coffee shop.',
+    image: {
+      url: 'https://storage.googleapis.com/material-design/publish/material_v_12/assets/0BxFyKV4eeNjDZUdpeURtaTUwLUk/style-color-colorsystem-gray-secondary-161116.png',
+      accessibilityText: 'Blue Grey Coffee Color',
+    },
+    display: 'WHITE',
+  },
+};
+
+// Handle the Dialogflow intent named 'favorite fake color'.
+// The intent collects a parameter named 'fakeColor'.
+app.intent('favorite fake color', (conv, {fakeColor}) => {
+  // Present user with the corresponding basic card and end the conversation.
+  conv.close(`Here's the color`, new BasicCard(colorMap[fakeColor]));
+});
